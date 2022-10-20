4d0e54da0032c63533f6212eac60f817849f75f5
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2022-10-20 15:31:30 -0600

    idpasoparametro

--- backend/src/models/Expresion/Id.py
+++ backend/src/models/Expresion/Id.py
@@ -55,39 +55,73 @@ class Id(Expresion):
         symbol:Symbol = ts.buscarC3d(self.id,tmp_aux)
         result = ValC3d(valor="0",isTemp=False,tipo=Tipos.ERROR)
         if symbol!=None:
-            tmpR= self.generator.newTemp()
-            index = self.generator.newTemp()
-            self.generator.addBackStack(index=tmp_aux)  # para retroceder entre enviroments
-            self.generator.addExpression(target=index,left="P",right=str(symbol.position),operator="+")
-            self.generator.addNextStack(tmp_aux)  # volver al enviroment actual de la pila
-            if self.paso_parametro == False:
-                self.generator.addGetStack(target=tmpR, index=index)
-            else:
-                tmpR = index
+            if self.paso_parametro == False: #CUANDO LO PIDEN SIN &
+                if symbol.paso_parametro == False:
+                    tmpR= self.generator.newTemp()
+                    index = self.generator.newTemp()
+                    self.generator.addBackStack(index=tmp_aux)  # para retroceder entre enviroments
+                    self.generator.addExpression(target=index,left="P",right=str(symbol.position),operator="+")
+                    self.generator.addNextStack(tmp_aux)  # volver al enviroment actual de la pila
+                    self.generator.addGetStack(target=tmpR, index=index)
 
-            if symbol.tsimbolo == Symbols.ARREGLO:
-                result.tipo_aux = Tipos.ARREGLO
-                result.prof_array = symbol.value.profundidad
-            elif symbol.tsimbolo == Symbols.VECTOR:
-                result.tipo_aux = Tipos.VECTOR
-                result.prof_array = symbol.value.profundidad
+                else: #SE DECLARO COMO PASO DE PARAMETRO EL SIMBOLO pero se requiere su valor
+                      #SE SACA PRIMERO EL VALOR DE DIRECCION EN EL STACK Y CON ESE VALOR SE SACA EL VALOR REQUERIO
+                    tmpR = self.generator.newTemp()
+                    index = self.generator.newTemp()
+                    self.generator.addBackStack(index=tmp_aux)  # para retroceder entre enviroments
+                    self.generator.addExpression(target=index, left="P", right=str(symbol.position), operator="+")
+                    self.generator.addNextStack(tmp_aux)  # volver al enviroment actual de la pila
+                    self.generator.addGetStack(target=tmpR, index=index)
+                    self.generator.addGetStack(target=tmpR, index=tmpR)
 
-            if symbol.tipo != Tipos.BOOLEAN or symbol.tsimbolo == Symbols.VECTOR or symbol.tsimbolo == Symbols.ARREGLO:
-                result.valor=tmpR
-                result.isTemp=True
-                result.tipo=symbol.tipo
-            else:
-                valor_r = ValC3d(valor="",isTemp= False,tipo= Tipos.BOOLEAN)
-                if (self.trueLabel == ""):
-                    self.trueLabel = self.generator.newLabel()
-                if (self.falseLabel == ""):
-                    self.falseLabel = self.generator.newLabel()
-                self.generator.addIf(tmpR, "1", "==", self.trueLabel)
-                self.generator.addGoto(self.falseLabel)
+                if symbol.tsimbolo == Symbols.ARREGLO:
+                    result.tipo_aux = Tipos.ARREGLO
+                    result.prof_array = symbol.value.profundidad
+                elif symbol.tsimbolo == Symbols.VECTOR:
+                    result.tipo_aux = Tipos.VECTOR
+                    result.prof_array = symbol.value.profundidad
 
-                valor_r.trueLabel = self.trueLabel
-                valor_r.falseLabel = self.falseLabel
-                result=valor_r
+                if symbol.tipo != Tipos.BOOLEAN or symbol.tsimbolo == Symbols.VECTOR or symbol.tsimbolo == Symbols.ARREGLO:
+                    result.valor = tmpR
+                    result.isTemp = True
+                    result.tipo = symbol.tipo
+                else:
+                    valor_r = ValC3d(valor="", isTemp=False, tipo=Tipos.BOOLEAN)
+                    if (self.trueLabel == ""):
+                        self.trueLabel = self.generator.newLabel()
+                    if (self.falseLabel == ""):
+                        self.falseLabel = self.generator.newLabel()
+                    self.generator.addIf(tmpR, "1", "==", self.trueLabel)
+                    self.generator.addGoto(self.falseLabel)
+
+                    valor_r.trueLabel = self.trueLabel
+                    valor_r.falseLabel = self.falseLabel
+                    result = valor_r
+            else:#CUANDO LO PIDEN CON &
+                if symbol.paso_parametro == False:#fue declarado de forma normal
+                    tmpR = self.generator.newTemp()
+                    self.generator.addBackStack(index=tmp_aux)  # para retroceder entre enviroments
+                    self.generator.addExpression(target=tmpR, left="P", right=str(symbol.position), operator="+")
+                    self.generator.addNextStack(tmp_aux)  # volver al enviroment actual de la pila
+                else: #si el simbolo fue declarado como un paso de parametro
+                    #SI EL SIMBOLO FUE DECLARADO COMO PASO DE PARAMETRO LO QUE SE GUARDO EN LA PILA: STACK[index] = val
+                    #FUE LA DIRECCION MANDADA, ENTONCES ES DE SUBSTRAER DICHA DIRECCION DE LA PILLA
+                    tmpR = self.generator.newTemp()
+                    index = self.generator.newTemp()
+                    self.generator.addBackStack(index=tmp_aux)  # para retroceder entre enviroments
+                    self.generator.addExpression(target=index, left="P", right=str(symbol.position), operator="+")
+                    self.generator.addNextStack(tmp_aux)  # volver al enviroment actual de la pila
+                    self.generator.addGetStack(target=tmpR, index=index)
+                if symbol.tsimbolo == Symbols.ARREGLO:
+                    result.tipo_aux = Tipos.ARREGLO
+                    result.prof_array = symbol.value.profundidad
+                elif symbol.tsimbolo == Symbols.VECTOR:
+                    result.tipo_aux = Tipos.VECTOR
+                    result.prof_array = symbol.value.profundidad
+
+                result.valor = tmpR
+                result.isTemp = True
+                result.tipo = symbol.tipo
         else:
             error = "no existe dicha id"
             print(error)
